93fc68d9807086261c08c12cd1816a1c6fd73987
yandrea888 <yandream888@gmail.com>
2020-10-15 22:54:31 -0500

    corrección en algoritmo 1

--- algoritmo1.py
+++ algoritmo1.py
@@ -1,5 +1,5 @@
 #Ejercicio 1
-base = float(input("La base del triangilo es: "))
-altura = float(input("La altura del triangilo es: "))
+base = float(input("La base del triángulo es: "))
+altura = float(input("La altura del triángulo es: "))
 area = base*altura/2
 print ("El area es {}." .format(area))
